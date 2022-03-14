82d8c62b79475d0020939b6f1b3a4312efa3675c
Matheus Diniz <matheus.diniiiiz@gmail.com>
2022-03-14 19:09:51 +0000

    Initial Commit

---
+++ DiceeRoll-SwiftUI/DiceeRoll_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeRoll_SwiftUIApp.swift
+//  DiceeRoll-SwiftUI
+//
+//  Created by Matheus Diniz on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct DiceeRoll_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
